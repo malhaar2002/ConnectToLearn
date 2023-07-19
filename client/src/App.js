9af154b87eaf1d45e9d0308621f0113be16ee1ea
malhaar2002 <aroramalhaar@gmail.com>
2023-07-19 20:11:32 +0530

    merged changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,34 @@
 import "./App.css";
 import { useState, useEffect } from "react";
+import ChatBot from "react-chatbot-kit";
+import 'react-chatbot-kit/build/main.css'
+import ActionProvider from "./chatbot/ActionProvider";
+import MessageParser from "./chatbot/MessageParser";
+import config from "./chatbot/config";
 
 function App() {
   const [data, setData] = useState([{}]);
 
-  useEffect(() => {
-    fetch("/message")
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-        console.log(data);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch("/message")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setData(data);
+  //       console.log(data);
+  //     });
+  // }, []);
 
-  const [input, setInput] = useState("");
-  const [messages, setMessages] = useState([]);
-
-  const handleSend = async () => {
-    const response = await fetch("/message", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ message: input }),
-    });
-    setMessages([
-      ...messages,
-      {text: input, isUser: true},
-      {text: response.data, isUser: false}
-    ]);
-    setInput("");
-    console.log(response.data)
-  };
-
-  return (
-    <div className="App">
-      <div className="chat">
-        <div className="user-message">This is a user message</div>
-        <div className="bot-message">This is a bot message</div>
-      </div>
-
-      <div className="input-container">
-        <input
-          type="text"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <button onClick={handleSend}>Send</button>
-      </div>
+ return (
+  <div className="App">
+    <div style={{ maxWidth: "300px" }}>
+      <ChatBot
+        config={config}
+        actionProvider={ActionProvider}
+        messageParser={MessageParser}
+      />
     </div>
-  );
+  </div>
+ ) 
 }
 
 export default App;
--- flask-server/app.py
+++ flask-server/app.py
@@ -5,8 +5,12 @@ app = Flask(__name__)
 
 @app.route('/message', methods=['POST'])
 def home():
-	project = request.get_json()['message']
+	project = request.get_json()["message"]
+	with open("log.txt", "w") as f:
+		f.write(project + "\n")
 	result = get_research(project)
+	# with open("log.txt", "a") as f:
+	# 	f.write(result + "\n")
 	return result
 
 if __name__ == '__main__':
