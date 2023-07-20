2867b422d4175c4ef36a66a11f69511f51815247
malhaar2002 <aroramalhaar@gmail.com>
2023-07-20 14:38:57 +0530

    integrated project recommender model with chatbot

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,17 +1,16 @@
 from flask import Flask, request
-from research import get_research
+from project_recommder_model import llm_chain
 
 app = Flask(__name__)
 
 @app.route('/message', methods=['POST'])
 def home():
-	project = request.get_json()["message"]
+	user_input = request.get_json()["user_message"]
 	with open("log.txt", "w") as f:
-		f.write(project + "\n")
-	result = get_research(project)
-	# with open("log.txt", "a") as f:
-	# 	f.write(result + "\n")
-	return result
+		f.write(user_input + "\n")
+	result = llm_chain.predict(human_input = user_input)
+	json_result = {"bot_message": result}
+	return json_result
 
 if __name__ == '__main__':
 	app.run(debug=True)
