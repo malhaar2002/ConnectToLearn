a12ac8e8bcb0cd14b73000e1efb870c844f06b7d
malhaar2002 <aroramalhaar@gmail.com>
2023-07-20 14:51:14 +0530

    removed log.txt

--- flask-server/app.py
+++ flask-server/app.py
@@ -6,8 +6,6 @@ app = Flask(__name__)
 @app.route('/message', methods=['POST'])
 def home():
 	user_input = request.get_json()["user_message"]
-	with open("log.txt", "w") as f:
-		f.write(user_input + "\n")
 	result = llm_chain.predict(human_input = user_input)
 	json_result = {"bot_message": result}
 	return json_result
