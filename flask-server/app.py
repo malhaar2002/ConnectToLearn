85e02c98f6cbc7a20b3f61a442adb6c688f18c7d
malhaar2002 <aroramalhaar@gmail.com>
2023-07-27 15:01:43 +0530

    separate LLM chains for each new session now working

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,12 +1,17 @@
 from flask import Flask, request
 import openai.error
-from project_recommder_model import llm_chain
+from project_recommder_model import create_llm_chain 
 
 app = Flask(__name__)
+llm_chains = {}
 
-@app.route('/message', methods=['POST'])
-def home():
+@app.route('/<string:userID>/message', methods=['POST'])
+def home(userID):
 	user_input = request.get_json()["user_message"]
+	llm_chain = llm_chains.get(userID, None)
+	if llm_chain is None:
+		llm_chain = create_llm_chain()
+		llm_chains[userID] = llm_chain
 	try:
 		result = llm_chain.predict(human_input = user_input)
 	except openai.error.InvalidRequestError:
@@ -15,6 +20,13 @@ def home():
 		result = "Sorry, something went wrong. If you are the developer, please check the logs for errors."
 	json_result = {"bot_message": result}
 	return json_result
+
+@app.route('/<string:userID>/leave', methods=['POST'])
+def handle_leave(userID):
+    if userID in llm_chains:
+        del llm_chains[userID]
+    return "User left"
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
